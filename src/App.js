5f1b3c2ebae88b89d6db9a2e43cc0caee4051390
sina dehvari <cnadhv7@gmail.com>
2023-12-04 15:48:26 +0330

    create footer component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Banner from "./components/Banner"
 import Cards from "./components/Cards";
 import Search from "./components/Search";
 import Logos from "./components/Logos";
+import Footer from "./components/Footer";
+
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +16,7 @@ class App extends Component {
           <Cards />
           <Search />
           <Logos />
+          <Footer />
       </div>
     )
   }
