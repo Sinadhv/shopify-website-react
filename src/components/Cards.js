98a761c4992bb8d256266df0147bca6980e5a8a4
sina dehvari <cnadhv7@gmail.com>
2023-12-20 17:48:06 +0330

    update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import styles from "./Cards.module.css";
 import Card from "./Card";
 import iphonex from "../images/iphone10.jpg";
+import iphone11 from "../images/iphone11.jpg";
+import iphone12 from "../images/iphone12.jpg";
+import s21 from "../images/s21.jpg";
 
 
 class Cards extends Component {
@@ -9,9 +12,9 @@ class Cards extends Component {
   return (
    <div className={styles.container}>
     <Card image ={iphonex} name= "Iphone X" cost="500 $" />
-    <Card image ={iphonex} name= "Iphone X" cost="500 $" />
-    <Card image ={iphonex} name= "Iphone X" cost="500 $" />
-    <Card image ={iphonex} name= "Iphone X" cost="500 $" />
+    <Card image ={iphone11} name= "Iphone 11" cost="700 $" />
+    <Card image ={iphone12} name= "Iphone 12" cost="900 $" />
+    <Card image ={s21} name= "S 21" cost="850 $" />
    </div>
   );
  }
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import styles from './Search.module.css'
 
 class Search extends Component {
+
+   constructor (){
+    super ();
+    this.state = {
+     text : ""
+    }
+
+   }
+
+   changeHandler = event =>{
+    this.setState({
+     text : event.target.value,
+    })
+   }
  render() {
   return (
    <div className={styles.container}>
     <p>Search What you want</p>
-    <input placeholder='Search...' />
+    <div>
+    <input value={this.state.text} onChange={this.changeHandler} type="text" placeholder='Search...' />
+    <br />
+    <br />
+    <span>{this.state.text}</span>
+    </div>
    </div>
   );
  }
